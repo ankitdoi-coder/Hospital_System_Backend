61a1328e7e567cf0066578da49cbc1fdd4242094
ankitdoi--coder <ankitdoi82@gmail.com>
2026-06-20 19:53:13 +0530

    integration

--- src/main/java/com/ankit/HealthCare_Backend/appointment/dto/AppointmentDTO.java
+++ src/main/java/com/ankit/HealthCare_Backend/appointment/dto/AppointmentDTO.java
@@ -1,37 +1,37 @@
 package com.ankit.HealthCare_Backend.appointment.dto;
 
 import java.time.LocalDate;
-import jakarta.validation.constraints.NotNull;
+
 import com.ankit.HealthCare_Backend.core.enums.AppointmentStatusEnum;
 import com.ankit.HealthCare_Backend.core.enums.BillingStatus;
 
-
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class AppointmentDTO {
     private Long id;
 
-    @NotNull(message="patient Id can not be null")
+    
     private Long patientId;
 
-    @NotNull(message="doctor id can not be null")
+   
     private Long doctorId;
 
-    @NotNull(message="doctor first name can not be null")
+  
     private String doctorFirstName;
 
-    @NotNull(message="doctor lastName can not be null")
+  
     private String doctorLastName;
 
-    @NotNull(message ="patient first name can not be null")
+   
     private String patientFirstName;
-    @NotNull(message="patient lastnamecan not be null")
+   
     private String patientLastName;
     
     private String doctorSpecialty;
 
-    @NotNull(message="appointment date can not be null ")
+    
     private LocalDate appointmentDate;
 
     private AppointmentStatusEnum status;
